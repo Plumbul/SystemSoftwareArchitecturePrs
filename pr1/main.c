2d19ef7e1e9685809f43ae5c65b93cff552b4604
Plumbul <154317854+Plumbul@users.noreply.github.com>
2026-03-14 10:53:43 +0200

    Update main.c

--- pr1/main.c
+++ pr1/main.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <pthread.h>
+#include "modules.h"
 
+int main() {
+    pthread_t thread1, thread2;
+    int initial_data = 100;
+
+    printf("Initial value: %d\n", initial_data);
+
+    pthread_create(&thread1, NULL, add_func, &initial_data);
+    pthread_create(&thread2, NULL, sub_func, &initial_data);
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    printf("Result: %d\n", initial_data);
+    return 0;
+}
