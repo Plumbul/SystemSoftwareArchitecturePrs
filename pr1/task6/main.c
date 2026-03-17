f531e98b1e0795d1f8aa48084c08825911d60ba2
Plumbul <154317854+Plumbul@users.noreply.github.com>
2026-03-17 16:25:04 +0200

    Update main.c

--- pr1/task6/main.c
+++ pr1/task6/main.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+#include <aio.h>
+#include <unistd.h>
+#include <pthread.h>
 #include "modules.h"
+
+void async_print(const char *msg) {
+    static struct aiocb cb;
+    memset(&cb, 0, sizeof(struct aiocb));
+    cb.aio_fildes = STDOUT_FILENO;
+    cb.aio_buf = (void *)msg;
+    cb.aio_nbytes = strlen(msg);
+
+    if (aio_write(&cb) == -1) {
+        perror("aio_write failed");
+    }
+}
 
 int main() {
     int initial_data = 100;
-    printf("Initial value: %d\n", initial_data);
+    char msg[64];
+    sprintf(msg, "Initial value: %d\n", initial_data);
+    async_print(msg);
 
-    add_func(&initial_data);
-    sub_func(&initial_data);
+    pthread_t t1, t2;
+    pthread_create(&t1, NULL, add_func, &initial_data);
+    pthread_create(&t2, NULL, sub_func, &initial_data);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
 
     printf("Result: %d\n", initial_data);
     return 0;
