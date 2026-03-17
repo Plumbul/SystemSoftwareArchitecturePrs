3e26c7bc2c9aa53fb1860ae7a0c733e2b347d4cf
Plumbul <154317854+Plumbul@users.noreply.github.com>
2026-03-17 16:23:12 +0200

    Update main.c

--- pr1/task6/main.c
+++ pr1/task6/main.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include "modules.h"
 
+int main() {
+    int initial_data = 100;
+    printf("Initial value: %d\n", initial_data);
+
+    add_func(&initial_data);
+    sub_func(&initial_data);
+
+    printf("Result: %d\n", initial_data);
+    return 0;
+}
